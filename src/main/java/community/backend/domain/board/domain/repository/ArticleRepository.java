9a35520ee1dd7e1024ab3c1004424c7cd9a9489a
Kuah_0 <gdsygrt557422@naver.com>
2024-01-21 17:54:55 +0900

    feat: article repository

---
+++ src/main/java/community/backend/domain/board/domain/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package community.backend.domain.board.domain.repository;
+
+import community.backend.domain.board.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
