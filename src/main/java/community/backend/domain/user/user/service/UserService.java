c0780d6e0917a2590870f992958daf5fb2319144
Kuah_0 <gdsygrt557422@naver.com>
2024-01-21 17:39:36 +0900

    fix: 원래대로 변경

--- src/main/java/community/backend/domain/user/user/service/UserService.java
+++ src/main/java/community/backend/domain/user/user/service/UserService.java
@@ -3,7 +3,6 @@ package community.backend.domain.user.user.service;
 import community.backend.domain.user.user.domain.User;
 import community.backend.domain.user.user.domain.repository.UserRepository;
 import community.backend.domain.user.user.exception.DuplicatedUserNameException;
-import community.backend.domain.user.user.exception.InternalServerException;
 import community.backend.domain.user.user.exception.PasswordMismatchException;
 import community.backend.domain.user.user.exception.UserNotFoundException;
 import community.backend.domain.user.user.presentation.dto.request.DeleteUserRequestDto;
@@ -23,80 +22,60 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public UserResponseDto getUserInfo(Long id) {
-        try {
-            User user = userRepository.findById(id)
-                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-            return UserResponseDto.of(user);
-        } catch (Exception e) {
-            throw InternalServerException.EXCEPTION;
-        }
+        return UserResponseDto.of(user);
     }
 
     @Transactional
     public void updateUserName(UpdateNameRequestDto dto, Long id) {
-        try {
-            User user = userRepository.findById(id)
-                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-            // 이름 중복 확인
-            if (userRepository.findByName(dto.getName()).isPresent()) {
-                throw DuplicatedUserNameException.EXCEPTION;
-            }
-
-            user.updateName(dto.getName());
-            userRepository.save(user);
-        } catch (Exception e) {
-            throw InternalServerException.EXCEPTION;
+        // 이름 중복 확인
+        if (userRepository.findByName(dto.getName()).isPresent()) {
+            throw DuplicatedUserNameException.EXCEPTION;
         }
+
+        user.updateName(dto.getName());
+        userRepository.save(user);
     }
 
     @Transactional
     public void updateUserPassword(UpdatePasswordRequestDto dto, Long id) {
-        try {
-            User user = userRepository.findById(id)
-                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-            // 비밀번호 일치 여부 확인
-            if (!passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword())) {
-                throw PasswordMismatchException.EXCEPTION;
-            }
-
-            user.updatePassword(passwordEncoder.encode(dto.getPassword()));
-            userRepository.save(user);
-        } catch (Exception e) {
-            throw InternalServerException.EXCEPTION;
+        // 비밀번호 일치 여부 확인
+        if (!passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword())) {
+            throw PasswordMismatchException.EXCEPTION;
         }
+
+        user.updatePassword(passwordEncoder.encode(dto.getPassword()));
+        userRepository.save(user);
     }
 
     @Transactional
     public void updateUserProfileImage(String profileImage, Long id) {
-        try {
-            // TODO: 나중에 S3로 변경
-            User user = userRepository.findById(id)
-                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        // TODO: 나중에 S3로 변경
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-            user.updateProfileImage(profileImage);
-            userRepository.save(user);
-        } catch (Exception e) {
-            throw InternalServerException.EXCEPTION;
-        }
+        user.updateProfileImage(profileImage);
+        userRepository.save(user);
     }
 
     @Transactional
     public void deleteUser(DeleteUserRequestDto dto, Long id) {
-        try {
-            User user = userRepository.findById(id)
-                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-            // 비밀번호 일치 여부 확인
-            if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
-                throw PasswordMismatchException.EXCEPTION;
-            }
-
-            userRepository.delete(user);
-        } catch (Exception e) {
-            throw InternalServerException.EXCEPTION;
+        // 비밀번호 일치 여부 확인
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            throw PasswordMismatchException.EXCEPTION;
         }
+
+        userRepository.delete(user);
     }
 }
