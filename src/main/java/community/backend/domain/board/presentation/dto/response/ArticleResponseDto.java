290bb67cd8635b82f77c9fca2820d8d7801175aa
Kuah_0 <gdsygrt557422@naver.com>
2024-02-02 12:59:40 +0900

    feat: list 형식 받는 of 추가

--- src/main/java/community/backend/domain/board/presentation/dto/response/ArticleResponseDto.java
+++ src/main/java/community/backend/domain/board/presentation/dto/response/ArticleResponseDto.java
@@ -1,9 +1,12 @@
 package community.backend.domain.board.presentation.dto.response;
 
+import community.backend.domain.board.domain.Article;
+import community.backend.domain.board.exception.article.ArticleNotFoundException;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -15,14 +18,24 @@ public class ArticleResponseDto {
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public static ArticleResponseDto of(Long id, String title, String content, String author, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public static ArticleResponseDto of(Article article) {
+        if (article == null) throw ArticleNotFoundException.EXCEPTION;
+
         return ArticleResponseDto.builder()
-                .id(id)
-                .title(title)
-                .content(content)
-                .author(author)
-                .createdDate(createdDate)
-                .modifiedDate(modifiedDate)
+                .id(article.getId())
+                .author(article.getAuthor())
+                .title(article.getTitle())
+                .content(article.getContent())
+                .createdDate(article.getCreatedDate())
+                .modifiedDate(article.getModifiedDate())
                 .build();
+    }
+
+    public static List<ArticleResponseDto> of(List<Article> articles) {
+        if (articles == null) throw ArticleNotFoundException.EXCEPTION;
+
+        return articles.stream()
+                .map(ArticleResponseDto::of)
+                .collect(java.util.stream.Collectors.toList());
     }
 }
--- src/main/java/community/backend/domain/board/presentation/dto/response/BoardResponseDto.java
+++ src/main/java/community/backend/domain/board/presentation/dto/response/BoardResponseDto.java
@@ -1,7 +1,12 @@
 package community.backend.domain.board.presentation.dto.response;
 
+import community.backend.domain.board.domain.Board;
+import community.backend.domain.board.exception.board.BoardNotFoundException;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -10,11 +15,21 @@ public class BoardResponseDto {
     private String name;
     private String description;
 
-    public static BoardResponseDto of(Long id, String name, String description) {
+    public static BoardResponseDto of(Board board) {
+        if (board == null) throw BoardNotFoundException.EXCEPTION;
+
         return BoardResponseDto.builder()
-                .id(id)
-                .name(name)
-                .description(description)
+                .id(board.getId())
+                .name(board.getName())
+                .description(board.getDescription())
                 .build();
+    }
+
+    public static List<BoardResponseDto> of(List<Board> boards) {
+        if (boards == null) throw BoardNotFoundException.EXCEPTION;
+
+        return boards.stream()
+                .map(BoardResponseDto::of)
+                .collect(Collectors.toList());
     }
 }
