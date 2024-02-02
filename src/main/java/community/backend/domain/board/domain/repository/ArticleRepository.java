46d6e8b40ef189110bd3001c375ca8846edc066d
Kuah_0 <gdsygrt557422@naver.com>
2024-02-03 00:41:30 +0900

    feat: repository

--- src/main/java/community/backend/domain/board/domain/repository/ArticleRepository.java
+++ src/main/java/community/backend/domain/board/domain/repository/ArticleRepository.java
@@ -1,9 +1,16 @@
 package community.backend.domain.board.domain.repository;
 
 import community.backend.domain.board.domain.Article;
+import community.backend.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByBoardOrderByCreatedAtDesc(Board board);
+
+    Optional<Article> findByIdAndBoard(Long id, Board board);
 }
